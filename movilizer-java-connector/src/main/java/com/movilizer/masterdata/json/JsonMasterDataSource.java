9f26caf16ba89d264cd51e5318868db14c183493
Peter Grigoriev <peter.grigoriev@gmail.com>
2015-04-23 10:27:11 +0200

    Json Master data reader now collects properties of nested objetcs

--- movilizer-java-connector/src/main/java/com/movilizer/masterdata/json/JsonMasterDataSource.java
+++ movilizer-java-connector/src/main/java/com/movilizer/masterdata/json/JsonMasterDataSource.java
@@ -5,9 +5,12 @@ import com.movilizer.masterdata.IMasterdataReaderResult;
 import com.movilizer.masterdata.IMasterdataSource;
 import com.movilizer.masterdata.IMasterdataXmlSetting;
 import com.movilizer.rest.INamedReaderProvider;
+import com.movilizer.util.logger.ComponentLogger;
+import com.movilizer.util.logger.ILogger;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
+import java.io.Reader;
 import java.sql.SQLException;
 import java.util.Collection;
 
@@ -16,17 +19,24 @@ import java.util.Collection;
  */
 public class JsonMasterDataSource implements IMasterdataSource {
     private final INamedReaderProvider jsonSource;
-    private final MasterdataJsonReader reader;
+    private final MasterdataJsonReader jsonReader;
+
+    private final ILogger logger = ComponentLogger.getInstance(JsonMasterDataSource.class);
 
     public JsonMasterDataSource(INamedReaderProvider jsonSource) {
         this.jsonSource = jsonSource;
-        reader = new MasterdataJsonReader();
+        jsonReader = new MasterdataJsonReader();
     }
 
     @Override
     public IMasterdataReaderResult read(IMasterdataXmlSetting setting) throws SQLException, XMLStreamException, IOException {
         String pool = setting.getPool();
-        return reader.read(jsonSource.get(pool + ".json").get(), setting);
+        Reader reader = jsonSource.get(pool + ".json").get();
+        if(reader == null) {
+            logger.error("Could not obtain reader for the pool [" + setting.getPool() + "]");
+            return null;
+        }
+        return jsonReader.read(reader, setting);
     }
 
     @Override
