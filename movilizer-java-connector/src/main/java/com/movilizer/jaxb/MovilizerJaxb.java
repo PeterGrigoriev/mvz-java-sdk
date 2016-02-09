0f78bd3548e91711f22f757c3c21bfeb59c0f6bb
Philippe Guillamet <philippe.guillamet@engie.com>
2016-02-09 15:27:48 +0100

    JAXBContext path upgraded to v14

--- movilizer-java-connector/src/main/java/com/movilizer/jaxb/MovilizerJaxb.java
+++ movilizer-java-connector/src/main/java/com/movilizer/jaxb/MovilizerJaxb.java
@@ -10,7 +10,7 @@ import javax.xml.bind.JAXBException;
  * @author Peter.Grigoriev@movilizer.com
  */
 public class MovilizerJaxb {
-    public static final String MOVILIZER_WSDL_V12 = "com.movilitas.movilizer.v12";
+    public static final String MOVILIZER_WSDL_V14 = "com.movilitas.movilizer.v14";
     protected static ILogger logger = ComponentLogger.getInstance("JaxbMarshalling");
     protected final JAXBContext jaxbContext;
 
@@ -21,7 +21,7 @@ public class MovilizerJaxb {
 
     private JAXBContext loadContext() {
         try {
-            return JAXBContext.newInstance(MOVILIZER_WSDL_V12);
+            return JAXBContext.newInstance(MOVILIZER_WSDL_V14);
         } catch (JAXBException e) {
             logger.fatal(e);
             return null;
